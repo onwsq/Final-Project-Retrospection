78eb004a812af7279009c4c1c00e3c7c15263b73
Olivia Weng <osw9@case.edu>
2021-04-19 19:45:01 -0400

    easy lock
    Co-Authored-By: gleverett <71728912+gleverett@users.noreply.github.com>

---
+++ Assets/Scripts/PinBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBehavior : MonoBehaviour
+{
+    [SerializeField] public Material newMat;
+    public float randomVal;
+    private float randomValPlus;
+
+    public int randTension;
+
+    private float randomValMinus;
+
+    private Rigidbody rb;
+
+    private Renderer ren;
+
+    private int tension;
+
+    public bool unlocked = false;
+
+    void Start()
+    {
+        randomVal = Random.Range(15.0f,25.0f);
+        randTension = Random.Range(0,5);
+        randomValPlus = randomVal + 0.5f;
+        randomValMinus = randomVal - 0.5f;
+        rb = this.GetComponent<Rigidbody>();
+        ren = this.GetComponent<Renderer>();
+        tension = 0;
+        unlocked = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         tension = TensionBehavior.tensionCount;
+         
+         if(this.transform.position.y <= randomValPlus && transform.position.y >= randomValMinus && tension == randTension){
+            ren.material = newMat;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            unlocked = true;}
+    }
+    
+    public bool isUnlocked() {
+        return unlocked;
+    }
+
+    }
+
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] public PinBehavior pin1;
+    [SerializeField] public PinBehavior pin2;
+    [SerializeField] public PinBehavior pin3;
+
+    public static bool won;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        won = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pin1.isUnlocked() && pin2.isUnlocked() && pin3.isUnlocked()) {
+            won = true;
+        }
+    }
+}
